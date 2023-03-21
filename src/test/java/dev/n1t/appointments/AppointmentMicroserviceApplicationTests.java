6dc7a380cc704638314f6e90ce835093f0d0c79e
John <jvt703@gmail.com>
2023-03-21 12:53:56 -0400

    Initial commit

---
+++ src/test/java/dev/n1t/appointments/AppointmentMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.n1t.appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
